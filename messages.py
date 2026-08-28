7d2863d97fbf71ff6339fbaa779fb79c59f62241
satapathynana420-sudo <satapathynana420@gmail.com>
2026-08-28 17:02:32 +0530

    added find_dotenv

--- messages.py
+++ messages.py
@@ -3,9 +3,9 @@ import streamlit as st
 import os
 from langchain_google_genai import ChatGoogleGenerativeAI
 st.header("HI")
-from dotenv import load_dotenv
+from dotenv import load_dotenv,find_dotenv
 
-load_dotenv()
+load_dotenv(find_dotenv())
 # api_key=os.getenv("GOOGLE_API_KEY")
 model=ChatGoogleGenerativeAI(
     model='gemini-3.6-flash',
