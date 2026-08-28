224840595d9bc67da99c10a54671fbe34081ec5b
subhranshu <satapathynana420@gmail.com>
2026-08-28 16:21:50 +0530

    added api key from .env file ro main file

--- messages.py
+++ messages.py
@@ -1,11 +1,12 @@
 from langchain_core.messages import HumanMessage,AIMessage,SystemMessage
 import streamlit as st
+import os
 from langchain_google_genai import ChatGoogleGenerativeAI
 st.header("HI")
 from dotenv import load_dotenv
 
 load_dotenv()
-
+api_key=os.getenv("GOOGLE_API_KEY")
 model=ChatGoogleGenerativeAI(model='gemini-3.6-flash')
 
 messages=[
