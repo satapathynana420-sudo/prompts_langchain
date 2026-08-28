1665a3f5a7b6c9da13f284e47948c866972ee6c1
subhranshu <satapathynana420@gmail.com>
2026-08-28 16:38:27 +0530

    api_key parameter added

--- messages.py
+++ messages.py
@@ -9,7 +9,7 @@ load_dotenv()
 # api_key=os.getenv("GOOGLE_API_KEY")
 model=ChatGoogleGenerativeAI(
     model='gemini-3.6-flash',
-    GOOGLE_API_KEY=os.getenv("GOOGLE_API_KEY"))
+    api_key=os.getenv("GOOGLE_API_KEY"))
 
 messages=[
     SystemMessage(content='You are a Helpful assistant'),
